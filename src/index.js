b124d4cdac917ced6e5251bb78519a7df23cf467
Kolawole Daniel <kolawolecoke@gmail.com>
2023-11-04 09:23:57 +0100

    created navigation context and provider to handle navigation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import NavigationProvider from "./context/navigation";
 
 const el = document.querySelector("#root");
 const root = createRoot(el);
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);

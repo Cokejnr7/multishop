dc6397d963aa651a920f256bccf729cc56ad210a
Kolawole Daniel <kolawolecoke@gmail.com>
2023-11-17 23:24:30 +0100

    featured prodcts

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,6 +1,24 @@
 import ProductList from "./ProductList";
 
 const FeaturedProducts = () => {
+  const [recentProducts, setRecentProducts] = useState([]);
+
+  const fetchRecentProducts = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/store/products/recent_products/"
+      );
+      console.log(response.data);
+      setRecentProducts(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecentProducts();
+  }, []);
+
   return <ProductList headerName={"FEATURED PRODUCTS"} />;
 };
 
